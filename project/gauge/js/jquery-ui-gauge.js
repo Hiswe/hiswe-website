f213d05925362dd6dff6c025a6f8a2f770c8d51e
Hiswe <hiswehalya@gmail.com>
2011-01-09 19:58:09 +0100

    [GAUGE] remove console.log & cleanup the code

--- project/gauge/js/jquery-ui-gauge.js
+++ project/gauge/js/jquery-ui-gauge.js
@@ -1,16 +1,15 @@
 /*
- * jQuery UI Tooltip @VERSION
+ * jQuery UI Gauge 0.5
  *
- * Copyright 2010, AUTHORS.txt (http://jqueryui.com/about)
+ * Copyright 2010, Hiswe (hiswehalya@gmail.com)
  * Dual licensed under the MIT or GPL Version 2 licenses.
  * http://jquery.org/license
  *
- * http://docs.jquery.com/UI/Tooltip
+ * http://docs.jquery.com/UI/ TODO : make doc
  *
  * Depends:
  *	jquery.ui.core.js
  *	jquery.ui.widget.js
- *	jquery.ui.position.js
  */
 (function($) {
 $.widget("ui.gauge", {
@@ -20,8 +19,7 @@ $.widget("ui.gauge", {
 		'easing' : "easeOutBounce" 
 	},
 	_create: function() {
-		this.$value = this.element.find('dd');		
-		console.log(this.percent);
+		this.$value = this.element.find('dd');
 		this.$barContainer = $('<dd />',{
 			'class' : 'gaugeValue',
 			'style' : 'height:'+this.options.height
@@ -35,7 +33,7 @@ $.widget("ui.gauge", {
 		this.$barContainer
 			.append(this.$bar)
 			.insertBefore(this.$value);
-			this.setPercent(parseInt(this.$value.text().replace(/^([0-9]*)%/g, '$1')));
+		this.setPercent(parseInt(this.$value.text().replace(/^([0-9]*)[%\.,]?[0-9]*[%\.,]?/g, '$1')));
 	},
 
 	enable: function() {
@@ -47,7 +45,7 @@ $.widget("ui.gauge", {
 	},
 
 	destroy: function() {
-		this$barContainer.remove()
+		this.$barContainer.remove();
 		$.Widget.prototype.destroy.apply(this, arguments);
 	},
 	setPercent: function(value){
