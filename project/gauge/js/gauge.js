47bf3f78373260b0441f3ae7fccc4910dd162269
Hiswe <hiswehalya@gmail.com>
2011-01-27 22:16:49 +0100

    [Gauge page] refactor js

--- project/gauge/js/gauge.js
+++ project/gauge/js/gauge.js
@@ -1,10 +1,37 @@
+var pageGauge = function(){
+	// data
+	var d = {};
+	// functions
+	var f = {
+		init : function () {
+			f._makeCache();
+			f._initGauge();
+			f._initSyntaxicColoration();
+		},
+		_initGauge : function () {
+			d.$gauge.gauge();
+			setTimeout(moveTo50,2000);
+			function moveTo50 () {
+				d.$gauge.eq(0).gauge('setPercent', 5);
+			}
+		},
+		_initSyntaxicColoration : function () {
+			//SyntaxHighlighter.config.tagName = 'pre';
+			SyntaxHighlighter.all();
+		},
+		_makeCache : function () {
+			d.$gauge = $('#jsGauge dl');
+		}
+	};
+	return {
+		init : f.init
+	};
+}();
+
+
+pageGauge.init();
+/*
 $(function(){
-	/*SyntaxHighlighter.config.tagName = 'pre';
-	SyntaxHighlighter.all();*/
-	var $gauge = $('#jsGauge dl');
-	$gauge.gauge();
-	setTimeout(moveTo50,2000);
-	function moveTo50 () {
-		$gauge.eq(0).gauge('setPercent', 5);
-	}
+	pageGauge.init();
 });
+//*/
