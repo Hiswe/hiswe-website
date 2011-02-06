33f069ecee449b5ae88185de87a6a982e071c667
Hiswe <hiswehalya@gmail.com>
2011-02-06 22:11:48 +0100

    [Inherit] create base objects

--- project/inheritance/js/inheritance.js
+++ project/inheritance/js/inheritance.js
@@ -3,5 +3,52 @@ var hiswe = {
 		function F() {}
         F.prototype = o;
         return new F();
-	}
+	},
+	out : function (message) {
+		this.$p.clone().text(message).appendTo(this.$test)
+	},
+	$test : $('#test'),
+	$p : $('<p />', {class : 'messageOut'})
 };
+
+var spore = function (spec) {	
+	
+	var that = {};
+	
+	that.getName = function () {
+		hiswe.out(spec.name);
+		return spec.name;
+	};
+	
+	that.noise = function () {
+		hiswe.out(spec.noise);	
+		return spec.noise || '';
+	};
+	
+	return that;
+};
+
+var pouic = spore({
+	name : 'pouic',
+	noise : 'pouiiiiiiiic'
+}),clapou = spore({
+	name : 'clopoo',
+	noise : 'aîe'
+});
+
+pouic.getName();
+pouic.noise();
+clapou.getName();
+clapou.noise();
+
+/*
+var constructor = function (spec, my) {
+	// spec === all argumemts to constructs a new instance
+	var that, privateInstanceVariable;
+	my = my || {};	
+	// add shared variables and functions to my
+	that = {}; // create a new object
+	// add privilegd methods to that
+	return that;
+}
+//*/
