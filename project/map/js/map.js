4ca2d9a66194fa1cdadba8e8c001b2f908c8e590
Yannick <yannick.aivayan@cafe.com>
2011-05-01 20:13:14 +0200

    [Map] refactor js

--- project/map/js/map.js
+++ project/map/js/map.js
@@ -2,8 +2,6 @@ h.module('game.world',{
 	options: {
 		width: 10,
 		height: 10,
-		cellWidth: 64,
-		cellHeight: 32,
 		id: 'world'
 	},
 	_create: function () {
@@ -17,30 +15,18 @@ h.module('game.world',{
 
 
 		this.world = world = [];
-		for (var i = 0; i < this.options.width; i++) {
+		for (var x = 0; x < this.options.width; x++) {
 			world.push([]);
-			for (var j = 0; j < this.options.height; j++) {
-				var currentCell = h.map.cell();
-				world[i].push(currentCell);
-				this.$element.append(currentCell.mapCell('build').css(this._positionCell(i,j)));
-
+			for (var y = 0; y < this.options.height; y++) {
+				var currentCell = h.map.cell({
+					worldWidth: this.options.width,
+					worldHeight: this.options.height
+				});
+				world[x].push(currentCell);
+				this.$element.append(currentCell.mapCell('build', x, y));
 			}
 		}
-		h.debug('table', world);
-	},
-	_positionCell: function(xCell, yCell) {
-		var top, left, worldWidth;
-		worldWidth = this.options.width * this.options.cellWidth;
-		top = (xCell * (this.options.cellHeight/2)) + (yCell * (this.options.cellHeight/2));
-		left = worldWidth/2 - ((this.options.cellWidth/2)*(xCell+1)) + (yCell * (this.options.cellWidth/2));
-
-
-
-		//h.debug('[',this.options.fullName,'] Position ::', left,'||',top);
-		return {
-			top: top+'px',
-			left: left+'px'
-		};
+		// h.debug('table', world);
 	}
 });
 
@@ -49,20 +35,31 @@ h.module('map.cell',{
 	options: {
 		width: 64,
 		height: 32,
-		template: '<div class="cell" />'
+		template: '<div href="" class="cell" />'
 	},
 	_create: function () {
-		// h.debug('info','[',this.options.fullName,'] Create');
+		this.options.worldWidth = this.options.worldWidth * this.options.width;
 	},
-	build: function () {
+	build: function (x, y) {
 		this.$cell = $(this.options.template)
 					.width(this.options.width)
 					.height(this.options.height);
+		this._positionCell(x, y);
 		if (Math.random() > 0.9) {
 			this.$cell.addClass('flower');
-		}					
+		}
 		//h.debug('[',this.options.fullName,'] build ::' ,this.$cell);
 		return this.$cell;
+	},
+	_positionCell: function (xCell, yCell) {
+		var top, left, worldWidth = this.options.worldWidth;
+		top = (xCell * (this.options.height/2)) + (yCell * (this.options.height/2));
+		left = worldWidth/2 - ((this.options.width/2)*(xCell+1)) + (yCell * (this.options.width/2));
+
+		this.$cell.css({
+			top: top+'px',
+			left: left+'px'
+		})
 	}
 });
 
