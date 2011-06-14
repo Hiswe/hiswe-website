906a4679ba90980f0e7da70a6026cc05206c6ba5
Yannick <yannick.aivayan@cafe.com>
2011-06-14 22:27:53 +0200

    [INHERITANCE] refactor

--- project/map/js/powerConstructor.js
+++ project/map/js/powerConstructor.js
@@ -1,7 +1,6 @@
 (function(h, $){
 	h.object('test.static',{
 		_create: function () {
-			h.debug('info','['+this.options.object+'] Create');
 			this._makeCache();
 			this._positionElement();
 			this._colorElement();
@@ -11,7 +10,6 @@
 			this.$element.appendTo(this.$parent);
 		},
 		_colorElement: function () {
-			h.debug('['+this.options.object+'] Color element');
 			function rand255 () {
 				return Math.round(255 * Math.random());
 			}
@@ -26,35 +24,26 @@
 			h.debug('['+this.options.object+'] color :', color.join(''));
 		},
 		_makeCache: function () {
-			h.debug('['+this.options.object+'] Make cache');
 			this.$parent = $('#frame');
 			this.$element  = $('<div />', {
 				'class': 'static'
 			});
 		},
 		_positionElement: function () {
-			h.debug('['+this.options.object+'] Position element');
-			var rand = this._randomPosition(),
-				left = rand.left,
-				top = rand.top;
+			var left = Math.round(this.$parent.width() * Math.random()),
+				top = Math.round(this.$parent.height() * Math.random());
 			h.debug('['+this.options.object+']','My position is :: x', left, '| y', top);
 			this.$element
 				.css({
 					left: left+'px',
 					top: top+'px'
 				})
-		},
-		_randomPosition: function () {
-			return {
-				left: Math.round(this.$parent.width() * Math.random()),
-				top: Math.round(this.$parent.height() * Math.random())
-			}
 		}
 	});
 }(hiswe, jQuery));
 
 (function(h, $){
-	h.object('test.transparent', 'test.static', {
+	h.object('test.transparent', 'test.static',{
 		_create: function () {
 			this._super('_create',arguments);
 			this._opacity();
@@ -75,27 +64,7 @@
 }(hiswe, jQuery));
 
 (function(h, $){
-	h.object('test.roundCorner', 'test.static', {
-		options: {
-			radius: 10
-		},
-		_create: function () {
-			this._super('_create',arguments);
-			this.$element.addClass('border');
-			this._roundCorner();
-		},
-		_roundCorner: function () {
-			this.$element.css({
-				"border-radius": this.options.radius,
-           		"-moz-border-radius": this.options.radius,
-           		"-webkit-border-radius": this.options.radius
-			});
-		}
-	});
-}(hiswe, jQuery));
-
-(function(h, $){
-	h.object('test.blinking', 'test.transparent', {
+	h.object('test.blinking', 'test.transparent',{
 		options: {
 			duration: 500
 		},
@@ -104,13 +73,12 @@
 			this._blink();
 		},
 		_setWidth: function () {
-			this._super('_setWidth');
 			this.$element.css({
-				width: '100px'
+				width: '50px',
+				height: '50px'
 			});
 		},
 		_blink: function () {
-			//h.debug('['+this.options.object+']','Blink');
 			this.$element.animate({
 				opacity: Math.random()
 			},{
@@ -121,50 +89,89 @@
 	});
 }(hiswe, jQuery));
 
-(function(h, $){
-	h.object('test.moving', 'test.transparent',{
-		_create: function () {
-			this._super('_create',arguments);
-			this._move();
-		},
-		_setWidth: function () {
-			this._super('_setWidth');
-			this.$element.css({
-				width: '75px'
-			});
-		},
-		_move: function () {
-			//h.debug('['+this.options.object+']','Blink');
-			rand = this._randomPosition();
-			this.$element.animate({
-				top: rand.top+'px',
-				left: rand.left+'px',
-			},{
-				duration: this.options.duration,
-				complete: $.proxy(this._move, this)
-			});
-		}
-	});
-}(hiswe, jQuery));
-(function(h, $){
 
+(function(h, $){
 	var pata = h.testStatic(),
 		pon = h.testStatic(),
 		foo = h.testTransparent(),
 		bar = h.testTransparent(),
 		wi = h.testBlinking({
 			duration: 300
-		}),
+		});
 		dget = h.testBlinking({
 			duration: 1100
-		}),
-		cracra = h.testMoving({
-			duration: 2000
-		}),
-		poui = h.testMoving({
-			duration: 300
-		}),
-		gloubi = h.testRoundCorner();
+		});
 
 
 }(hiswe, jQuery));
+
+/*
+(function(h, $){
+		// équivalent à $.extend mais avec une fonction super
+		// le super de test_transparent appele tes.roundCorner même si test.transparent à besoin de test.static
+		// décorateur = surcouche de fonction
+
+	h.object('test.priseDeTete', 'decorator.transparent', 'decorator.roundCorner', 'test.static');
+}(hiswe, jQuery));
+
+
+(function(h, $){
+	h.object('test.chouette', 'decorator.blinking', 'test.priseDeTete', {
+
+	});
+}(hiswe, jQuery));
+
+
+(function(h, $){
+		// équivalent à $.extend mais avec une fonction super
+		// le super de test_transparent appele tes.roundCorner même si test.transparent à besoin de test.static
+		// suppression du reste de la chaîne ...
+
+		// Regarder les mixins
+	 	// http://addyosmani.com/resources/essentialjsdesignpatterns/book/#designpatternsjavascript
+
+	h.object('test.bidule', 'test.priseDeTete', {
+		_create: function () {
+			this._super('_create',arguments);
+			this._opacity();
+			this._setWidth();
+		},
+		_setWidth: function () {
+			this.$element.css({
+				width: '40px',
+				height: '40px'
+			});
+		},
+		_opacity: function () {
+			this.$element.css({
+				opacity: Math.random()
+			})
+		}
+	});
+}(hiswe, jQuery));
+
+
+(function(h, $){
+	h.object('test.blinkingRound', 'test.blinkingRound' , 'test.transparentRound',{
+		options: {
+			duration: 500
+		},
+		_create: function () {
+			this._decorator('test.static');
+			this._decorator('test.blinking');
+			this._decorator('test.transparent');
+			this._decorator('test.round');
+
+				// my glorious code
+		}
+	});
+}(hiswe, jQuery));
+
+
+(function(h, $){
+
+	test.pouic = h.decorate('test.static', 'test.round' ,'test.transparent', {
+
+	});
+}(hiswe, jQuery));
+//*/
