44abb56c7ce3ea7837d20ebd0cda05aeac5ee271
Yannick <yannick.aivayan@cafe.com>
2011-06-25 20:33:37 +0200

    [MOVEABLE] first movement !

--- project/map/js/map.moveable.js
+++ project/map/js/map.moveable.js
@@ -1,14 +1,25 @@
 
-(function(h, $) {
-	h.object( 'map.moveable', 'map.static',{
-		_bindActions: function () {
-
-		},
+(function( h, $ ) {
+	h.object( 'map.moveable', 'map.static', {
 		_build: function () {
 			this._super( '_build', arguments );
 			var character = ( Math.random() > 0.5 ) ? 'flora' : 'celesta';
 			this.$cell.addClass( character );
+			var time = this._moveDelay();
+			setTimeout( $.proxy( this._move, this ), time );
+		},
+		_move: function () {
+			var that = this;
+			this.$cell.animate({
+				left: '+='+this.options.general.width
+			},
+			5000,
+			function () {
+				setTimeout( $.proxy( that._move, that ), that._moveDelay() );
+			});
+		},
+		_moveDelay: function () {
+			return Math.round( Math.random() * 10000 );
 		}
-
 	});
-}(hiswe, jQuery));
+}( hiswe, jQuery ) );
