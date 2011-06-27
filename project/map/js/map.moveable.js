2d4951cfd39fb230a01fc21c15c0cc6726c0679a
Yannick <yannick.aivayan@cafe.com>
2011-06-27 22:00:57 +0200

    [MOVEABLE] add a move method

--- project/map/js/map.moveable.js
+++ project/map/js/map.moveable.js
@@ -1,25 +1,84 @@
 
 (function( h, $ ) {
 	h.object( 'map.moveable', 'map.static', {
+		options: {
+			speed: 1000
+		},
+		_create: function () {
+			this._super( '_create', arguments );
+			this.directions = [ 'top', 'right', 'bottom', 'left' ];
+		},
+		_action: function () {
+			var that = this;
+			setTimeout( function () {
+				that._move( that._randomDirection(), that._action )
+			}, that._moveDelay() );
+
+		},
 		_build: function () {
 			this._super( '_build', arguments );
 			var character = ( Math.random() > 0.5 ) ? 'flora' : 'celesta';
 			this.$cell.addClass( character );
-			var time = this._moveDelay();
-			setTimeout( $.proxy( this._move, this ), time );
+			this._action();
 		},
-		_move: function () {
-			var that = this;
-			this.$cell.animate({
-				left: '+='+this.options.general.width
-			},
-			5000,
-			function () {
-				setTimeout( $.proxy( that._move, that ), that._moveDelay() );
-			});
+		_move: function ( direction, callback ) {
+			var that = this,
+				otherMove = $.map( this.directions, function ( value , index) {
+					return ( value != direction ) ? value : null;
+				});
+
+			//  h.debug( otherMove );
+			switch ( direction ) {
+				case 'top':
+					this.$cell.animate({
+						top: '+='  + this.options.general.height / 2,
+						left: '+=' + this.options.general.width / 2
+					},
+					this.options.speed,
+					function () {
+						callback.call( that );
+					});
+				break;
+				case 'right':
+					this.$cell.animate({
+						top: '-='  + this.options.general.height / 2,
+						left: '+=' + this.options.general.width / 2
+					},
+					this.options.speed,
+					function () {
+						callback.call( that );
+					});
+				break;
+				case 'bottom':
+					this.$cell.animate({
+						top: '-='  + this.options.general.height / 2,
+						left: '-=' + this.options.general.width / 2
+					},
+					this.options.speed,
+					function () {
+						callback.call( that );
+					});
+				break;
+				case 'left':
+					this.$cell.animate({
+						top: '-='  + this.options.general.height / 2,
+						left: '-=' + this.options.general.width / 2
+					},
+					this.options.speed,
+					function () {
+						callback.call( that );
+					});
+				break;
+				default:
+					h.debug( direction );
+			}
 		},
 		_moveDelay: function () {
 			return Math.round( Math.random() * 10000 );
+		},
+		_randomDirection: function () {
+			return this.directions[ Math.floor( Math.random() * this.directions.length ) ];
+
 		}
 	});
 }( hiswe, jQuery ) );
