d63756aee69ec0d3efa7fdce8899935fa9cf4a76
Hiswe <hiswehalya@gmail.com>
2014-04-01 23:00:53 +0700

    [GULP] add task to put rev in files

--- config/asset.coffee
+++ config/asset.coffee
@@ -1,9 +1,11 @@
-express = require 'express'
-expose  = require 'express-expose'
-path    = require 'path'
-stylus  = require 'stylus'
-nib     = require 'nib'
-hstrap  = require 'hstrap'
+express   = require 'express'
+expose    = require 'express-expose'
+path      = require 'path'
+stylus    = require 'stylus'
+nib       = require 'nib'
+hstrap    = require 'hstrap'
+
+stylusVar = require './stylus_var.json'
 
 activeClass     = 'hw-panel-active'
 activeBody      = 'hw-body-active'
@@ -22,11 +24,6 @@ module.exports = (app) ->
   app.use express.static(assets, {maxAge: maxAge})
 
   # expose some datas to the front app under hw namespace
-  options = {
-    activeClass:    activeClass
-    carrouselClass: carrouselClass
-    activeBody:     activeBody
-  }
-  app.expose({ options: options}, 'hw')
+  app.expose({ options: stylusVar}, 'hw')
 
   return app
--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var gulp        = require('gulp');
 var lr          = require('tiny-lr'); // livereload depend on tiny-lr
 var rev         = require('gulp-rev');
@@ -11,22 +13,37 @@ var concat      = require('gulp-concat');
 var rename      = require('gulp-rename');
 var notify      = require('gulp-notify');
 var resize      = require('gulp-image-resize');
+var replace     = require('gulp-replace'); // use to fix  rev manifest https://github.com/sindresorhus/gulp-rev/pull/18
 var nodemon     = require('gulp-nodemon');
+var minifyCSS   = require('gulp-minify-css');
 var livereload  = require('gulp-livereload');
-// https://www.npmjs.org/package/gulp-rev/
+var runSequence = require('run-sequence'); // Wait for orchestrator to be fixed https://www.npmjs.org/package/run-sequence
 
 var server = lr();
 
 var path = {
-  libs: ['bower_components/modernizr/modernizr.js',
+  libs: [
+    'bower_components/modernizr/modernizr.js', // used by js
     'bower_components/jquery/dist/jquery.js',
     'bower_components/hevent/build/jquery.hevent.js'],
   imgSrc: 'public/media/source/*',
   imgDst: 'public/media/images/'
 };
 
-var stylus_var = require('./stylus_var.json');
+var stylus_var = require('./config/stylus_var.json');
 stylus_var.isDev = true;
+
+
+// Bump json version
+gulp.task('bump', function () {
+  return gulp.src(['./package.json', './bower.json'])
+    .pipe(bump())
+    .pipe(gulp.dest('./'));
+});
+
+/////////
+// ASSETS
+/////////
 
 // Copy font
 gulp.task('clean-font', function() {
@@ -39,31 +56,46 @@ gulp.task('font', ['clean-font'], function() {
     .pipe(gulp.dest('public/media/'));
 });
 
-// Bump json version
-gulp.task('bump', function () {
-  return gulp.src(['./package.json', './bower.json'])
-    .pipe(bump())
-    .pipe(gulp.dest('./'));
+// Revision
+gulp.task('rev', function() {
+  gulp.src(['public/*.css', '!public/*-*.css', 'public/*.js', '!public/*-*.js'])
+  .pipe(rev())
+  .pipe(gulp.dest('public'))
+  .pipe(rev.manifest())     // generate a revision manifest file
+  .pipe(gulp.dest('config'))
+  .pipe(replace(/(.*)(:\s")\/(.*)/gi, '$1$2$3'))
+  .pipe(gulp.dest('config'))
+  .pipe(livereload(server));
 });
 
 // Stylus
 gulp.task('stylus', function () {
-  gulp.src('./assets/css/front/index.styl')
+  return gulp.src('./assets/css/front/index.styl')
     .pipe(stylus({use: ['nib', 'hstrap'], define: stylus_var}))
     .pipe(gulp.dest('./public'))
-    .pipe(livereload(server))
-    .pipe(notify({title: 'Stylus', message: 'CSS build'}));
+    .pipe(rename('index.min.css'))
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('./public'))
+    .pipe(notify({title: 'Stylus', message: 'CSS build done'}));
+});
+
+gulp.task('css', function(callback) {
+  runSequence('stylus', 'rev', callback);
 });
 
 // Concat & compress lib
 gulp.task('lib', function() {
-  gulp.src(path.libs)
+  return gulp.src(path.libs)
     .pipe(concat('lib.js'))
     .pipe(gulp.dest('public'))
     .pipe(rename('lib.min.js'))
     .pipe(uglify({mangle: false}))
     .pipe(gulp.dest('public'))
-    .pipe(notify({title: 'LIB', message: 'build done'}));
+    .pipe(notify({title: 'Lib', message: 'build done'}));
+});
+
+gulp.task('js', function(callback) {
+  runSequence('lib', 'rev', callback);
 });
 
 // Resize images
@@ -79,21 +111,25 @@ gulp.task('resize', ['clean-image'], function() {
     .pipe(gulp.dest(path.imgDst))
 });
 
+// build for production
+gulp.task('build', function(callback) {
+  runSequence(['lib', 'css'], 'rev', callback);
+});
+
+/////////
+// SERVER
+/////////
+
 // Watch
 gulp.task('watch', function() {
   server.listen(35729, function (err) {
     if (err) { return console.log(err) }
   });
-  gulp.watch(['./assets/css/front/**/*.styl'], ['stylus']);
+  gulp.watch(['./assets/css/front/**/*.styl'], ['css']);
   gulp.watch('./views/**/*.jade').on('change', function(file) {
-    // console.log(file.path);
     // server.changed(file.path);
     gulp.src('').pipe(notify({title: 'Hiswe server', message: 'reload html'}));
-    server.changed({
-      body: {
-        files: ['index.html']
-      }
-    });
+    server.changed({body: {files: ['index.html']}});
   });
 });
 
@@ -111,13 +147,17 @@ gulp.task('express', function () {
 
 gulp.task('server', ['watch','express']);
 
-// Light doc
+/////////
+// DOC
+/////////
+
 gulp.task('default', function() {
   console.log(gutil.colors.red('bump'), '   ', 'bump version of json');
   console.log(gutil.colors.red('font'), '   ', 'Copy fonts to the right folder');
-  console.log(gutil.colors.red('lib'), '    ', 'Concat & uglify libs');
+  console.log(gutil.colors.red('js'), '     ', 'Concat & uglify + rev');
+  console.log(gutil.colors.red('css'), '    ', 'Compile stylus + uglify + rev');
+  console.log(gutil.colors.red('build'), '  ', 'js + css + rev');
   console.log(gutil.colors.red('resize'), ' ', 'Resize images');
-  console.log(gutil.colors.red('stylus'), ' ', 'Compile stylus');
   console.log(gutil.colors.red('express'), '', 'Start server');
   console.log(gutil.colors.red('watch'), '  ', 'Watch stylus');
   console.log(gutil.colors.red('server'), ' ', 'Watch + server');
