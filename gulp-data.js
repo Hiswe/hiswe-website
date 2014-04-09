909255d51883d29f38e43704d79ba4a306f75242
Hiswe <hiswehalya@gmail.com>
2014-04-09 15:50:51 +0700

    [DATAS] add cover image to json

--- gulp-data.js
+++ gulp-data.js
@@ -1,13 +1,15 @@
-var Q           = require('q');
-var fs          = require('fs');
-var marked      = require('marked');
-var path        = require('./gulp-path.js');
+var Q             = require('q');
+var fs            = require('fs');
+var path          = require('./gulp-path.js');
+var gutil         = require('gulp-util');
+var marked        = require('marked');
 
-var jsonDb      = [];
+var jsonDb        = [];
+var openFile      = Q.denodeify(fs.open);
 
-var renderer = new marked.Renderer();
-
-renderer.heading = function (text, level) {
+// Markdown config
+var renderer      = new marked.Renderer();
+renderer.heading  = function (text, level) {
   var escapedText = text.replace(/-/gi, ' ');
   return '<h' + level + '>' + escapedText + '</h' + level + '>';
 };
@@ -17,22 +19,58 @@ marked.setOptions({
   smartypants: true
 });
 
-var parseFile = function readFile(fileName, data) {
+
+// Other things
+var setCover = function setCover(fileName) {
+  var deferred  = Q.defer();
+  fileName = 'cover-' + fileName[2];
+  var relativePath  = path.imgDst + fileName
+  var absolutePath  = __dirname + '/' + path.imgDst + fileName
+  openFile(absolutePath + '.png', 'r')
+    .then(function () {
+      return relativePath + '.png';
+    }, function (err){
+      return openFile(absolutePath + '.svg', 'r')
+    })
+    .then(function (file){
+      if(typeof file === 'number') {
+        return deferred.resolve(relativePath + '.svg');
+      }
+      return deferred.resolve(file);
+    }, function (err){
+      return deferred.reject('no such files' + relativePath + '.png|svg');
+    }).done();
+
+  return deferred.promise;
+};
+
+var parseFile = function readFile(fileName, data, deferred) {
   fileName = fileName.match(/^(\d*)-(.*).md$/)
-  jsonDb.push({
-    order: ~~fileName[1],
-    id: 'project-' + fileName[2],
-    name: fileName[2].replace(/-/gi, ' '),
-    markup: marked(data, {sanitize: true})
-  });
+
+  setCover(fileName)
+    .then(function (coverImage) {
+      jsonDb.push({
+        order: ~~fileName[1],
+        id: 'project-' + fileName[2],
+        name: fileName[2].replace(/-/gi, ' '),
+        cover: coverImage,
+        markup: marked(data, {sanitize: true})
+      });
+      return deferred.resolve();
+    }, function (err){
+      deferred.reject(err);
+    });
 };
 
 var walkFiles = function walkFiles(files) {
-  return files.filter(function(fileName){
+  var mdFiles = files.filter(function(fileName){
     return /.md$/.test(fileName);
-  }).map(function (fileName) {
-    return Q.npost(fs, 'readFile', [path.datas + '/' + fileName, {encoding: 'utf8'}])
-      .then(function(data){ parseFile(fileName, data)});
+  });
+  return mdFiles.map(function (fileName) {
+    var deferred  = Q.defer();
+    Q.npost(fs, 'readFile', [path.datas + '/' + fileName, {encoding: 'utf8'}])
+      .then(function(data){ parseFile(fileName, data, deferred)});
+    return deferred.promise
   });
 };
 
@@ -46,7 +84,9 @@ var bundleData = function bundleData(){
     .done(function(){
       jsonDb.sort(function(a, b){ return  a.order < b.order});
       fs.writeFileSync(path.jsonDb, JSON.stringify(jsonDb, null, 2));
-      deferred.resolve();
+      return deferred.resolve();
+    }, function(err){
+      return deferred.reject(new gutil.PluginError('json', err));
     });
 
   return deferred.promise
