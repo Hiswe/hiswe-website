faa02f3a500a18013c26afe619abbd9d5c11e2fe
Hiswe <hiswehalya@gmail.com>
2018-06-06 17:38:57 +0700

    DEV – add a watch task

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@
 
 const gulp = require('gulp')
 const $ = require('gulp-load-plugins')()
+const browserSync = require(`browser-sync`).create()
+const reload = browserSync.reload
 
 ////////
 // CSS
@@ -18,6 +20,43 @@ const css = () => {
     .pipe($.postcss([autoprefixer()]))
     .pipe($.rename(`hiswe.css`))
     .pipe(gulp.dest(`server/public`))
+    .pipe(browserSync.stream())
 }
 
 gulp.task(`css`, css)
+
+////////
+// DEV
+////////
+
+const nodemon = require('nodemon')
+const nodemonConfig = require('./nodemon.json')
+
+const build = css
+
+gulp.task(`build`, build)
+
+const watch = done => {
+  gulp.watch(`front/scss/*.scss`, css)
+  gulp.watch(`server/views/*.pug`, reload)
+  done()
+}
+
+const nodemonServer = done => {
+  nodemon(nodemonConfig).once(`start`, done)
+}
+
+const browserSyncServer = done => {
+  browserSync.init({
+    proxy: `http://localhost:3456`,
+    open: false,
+    port: 7000,
+    ghostMode: false,
+  })
+  done()
+}
+
+gulp.task(
+  `dev`,
+  gulp.series(build, gulp.parallel(watch, nodemonServer), browserSyncServer)
+)
