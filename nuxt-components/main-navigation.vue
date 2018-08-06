de3bd72a69c6cdb05e36b9799d4dd536e4297c91
Hiswe <hiswehalya@gmail.com>
2018-08-06 13:22:39 +0700

    NAVIGATION – remove hanurger menu on small devices

--- nuxt-components/main-navigation.vue
+++ nuxt-components/main-navigation.vue
@@ -1,12 +1,9 @@
 <template lang="pug">
 nav.main-navigation
-  input(type="checkbox" id="menu-toggle" v-model="open")
-  label(for="menu-toggle"): hiswe-icon(name="menu")
-  div(@click="close")
-    nuxt-link(to="/" exact-active-class="active" data-name="home") home
-    nuxt-link(to="/projects" active-class="active" data-name="projects") projects
-    // nuxt-link(to="/work" active-class="active" data-name="work") work
-    nuxt-link(to="/blog" active-class="active" data-name="blog") blog
+  nuxt-link(to="/" exact-active-class="active" data-name="home") home
+  nuxt-link(to="/projects" active-class="active" data-name="projects") projects
+  // nuxt-link(to="/work" active-class="active" data-name="work") work
+  nuxt-link(to="/blog" active-class="active" data-name="blog") blog
 </template>
 
 <style lang="scss" scoped>
@@ -15,56 +12,9 @@ nav.main-navigation
   --navigation-top-spacing: var(--gutter);
   font-weight: 800;
   background: var(--c-primary-lightest);
-}
-label {
-  --size: 40px;
-  color: white;
-  background: var(--c-accent);
-  width: var(--size);
-  height: var(--size);
-  display: block;
-  position: absolute;
-  top: 0;
-  left: 0;
-  z-index: 105;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
 
-  @media #{$mq-medium} {
-    display: none;
-  }
-}
-input {
-  @include hidden-accessible;
-
-  @media #{$mq-small} {
-    &:not(:checked) ~ div {
-      @include hidden-accessible;
-    }
-    &:checked + label {
-      position: fixed;
-    }
-  }
-}
-svg {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-}
-div {
-  position: fixed;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  background: white;
-  z-index: 100;
-
-  @media #{$mq-medium} {
-    position: static;
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    // width: 100%;
-  }
   @media #{$mq-big} {
     height: auto;
     background: var(--c-primary-lightest);
@@ -80,42 +30,22 @@ a {
   justify-content: center;
   position: relative;
 
-  @media #{$mq-small} {
-    width: 50%;
-    height: 50vh;
-    float: left;
-  }
-  @media #{$mq-medium} {
-    height: var(--nav-height);
-    text-align: center;
-    position: relative;
-    flex-grow: 1;
+  height: var(--nav-height);
+  text-align: center;
+  position: relative;
+  flex-grow: 1;
 
-    &:not(.active):hover::after {
-      clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%);
-
-      @media #{$mq-medium} {
-        clip-path: polygon(0 100%, 0 0, 100% 0, 100% 100%);
-      }
-    }
-    &.active::before {
-      content: '';
-      width: 10px;
-      position: absolute;
-      top: 0;
-      bottom: 0;
-      left: 0;
-      background: var(--c-accent);
-    }
+  &:not(.active):hover::after {
+    clip-path: polygon(0 100%, 0 0, 100% 0, 100% 100%);
   }
-  @media #{$mq-medium} {
-    &.active::before {
-      bottom: 0;
-      top: auto;
-      right: 0;
-      width: auto;
-      height: 0.5rem;
-    }
+  &.active::before {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background: var(--c-accent);
+    height: 0.5rem;
   }
 
   @media #{$mq-big} {
@@ -142,32 +72,15 @@ a {
     align-items: center;
     justify-content: center;
     color: var(--c-background);
-    background: var(--c-accent);
-    clip-path: polygon(0 0, 0 0, 0 100%, 0 100%);
+    background: var(--c-primary-darker);
+    clip-path: polygon(0 100%, 0 100%, 100% 100%, 100% 100%);
     transition: clip-path 0.25s;
-
-    @media #{$mq-medium} {
-      background: var(--c-primary-darker);
-      clip-path: polygon(0 100%, 0 100%, 100% 100%, 100% 100%);
-    }
   }
 }
 </style>
 
-
 <script>
 export default {
   name: `main-navigation`,
-  data() {
-    return {
-      open: false,
-    }
-  },
-  methods: {
-    close() {
-      this.open = false
-    },
-  },
 }
 </script>
-
