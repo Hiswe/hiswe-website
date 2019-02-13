d3f62ae2ae62897d4575dab798129e81335b5151
hiswe <hiswehalya@gmail.com>
2019-02-13 14:40:31 +0100

    RELEASE – version 4.1.2

---
+++ .nuxt/dist/server/e9ef11e4c9f2809b10d4.js
@@ -0,0 +1,2 @@
+exports.ids=[4],exports.modules={123:function(t,e,r){"use strict";r.r(e);var n={name:"page-work",transition:"page",head:{title:"work"}},o=r(1),component=Object(o.a)(n,function(){var t=this.$createElement,e=this._self._c||t;return e("hiswe-main-content",{attrs:{page:"work"}},[e("template",{slot:"header"},[e("hiswe-title",{attrs:{text:"wo-rk"}})],1)],2)},[],!1,null,null,"e423db9e");e.default=component.exports}};
+//# sourceMappingURL=e9ef11e4c9f2809b10d4.js.map
