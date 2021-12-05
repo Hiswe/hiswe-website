6fa78402fc473de9433434ff8ea1d52f645ce4d4
Yannick Aivayan <yannick.aivayan@visable.com>
2021-12-05 11:24:22 +0100

    refactor: remove koa server

--- server/services/index.js
+++
@@ -1,8 +0,0 @@
-import dotenv from 'dotenv'
-import sendGrid from '@sendgrid/mail'
-
-dotenv.config();
-
-sendGrid.setApiKey(process.env.SENDGRID_API_KEY)
-
-export const sendMail = (msg) => sendGrid.send(msg)
