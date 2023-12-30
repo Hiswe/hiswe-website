d731e5bb690adc8eeae0f197ffc8935c229591ec
Hiswe <hiswehalya@gmail.com>
2023-12-30 13:04:22 +0100

    refactor(tech): update icons

--- components/home/techs/build.vue
+++ components/home/techs/build.vue
@@ -6,11 +6,11 @@ defineOptions({
 
 <template>
   <div class="grid grid-cols-[1fr_5.5rem] gap-x-8 gap-y-6">
-    <UiTwoLineTitle class="p-0 text-start md:text-end col-span-2" text="bu-ild" />
+    <UiTwoLineTitle class="p-0 text-start md:text-end" text="bu-ild" />
     <HomeTechsItem name="vite" class="justify-self-end" href="https://vitejs.dev/">
       <SvgoTechVite />
     </HomeTechsItem>
-    <HomeTechsItem name="webpack" href="https://webpack.js.org">
+    <HomeTechsItem name="webpack" class="col-span-2 justify-self-end" href="https://webpack.js.org">
       <SvgoTechWebpack />
     </HomeTechsItem>
   </div>
