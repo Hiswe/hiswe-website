62372348145599057a68ab2bf6e4014619784e37
Hiswe <hiswehalya@gmail.com>
2023-12-30 13:32:35 +0100

    refactor(tech): split framework

--- components/home/techs/framework.vue
+++ components/home/techs/framework.vue
@@ -5,12 +5,12 @@ defineOptions({
 </script>
 
 <template>
-  <div class="grid grid-cols-[1fr_8rem] gap-x-8 gap-y-6">
+  <div class="grid grid-cols-[1fr_5.5rem] gap-x-8 gap-y-6 justify-items-end">
     <UiTwoLineTitle class="p-0" text="frame-work" />
     <HomeTechsItem name="vue" href="https://vuejs.org/">
       <SvgoTechVue />
     </HomeTechsItem>
-    <HomeTechsItem name="nuxt" long class="justify-self-end" href="https://nuxt.com/">
+    <HomeTechsItem name="nuxt" long href="https://nuxt.com/">
       <SvgoTechNuxt />
     </HomeTechsItem>
     <HomeTechsItem name="vitest" href="https://vitest.dev/">
